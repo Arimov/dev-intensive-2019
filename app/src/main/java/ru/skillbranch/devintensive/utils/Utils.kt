171a68b2ea1c274f0425c91da65b878b7f001d57
Sergey Arimov <arimov@arimsoft.ru>
2019-06-30 10:11:01 +0300

    toInitials method added to Utils

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,8 +1,8 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
-    fun ParseFullName(fullName : String?):Pair<String?, String?>{
-        var parts : List<String>? = fullName?.trim()?.split(" ")
+    fun ParseFullName(fullName: String?):Pair<String?, String?>{
+        val parts : List<String>? = fullName?.trim()?.split(" ")
         var firstName = parts?.getOrNull(0)
         if(firstName.isNullOrEmpty() || firstName.isNullOrBlank()) {
             firstName = null
@@ -12,5 +12,24 @@ object Utils {
             lastName = null
         }
         return firstName to lastName
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String?{
+        val result: String
+        var firstSymbol = ""
+        var lastSymbol = ""
+        if(!firstName.isNullOrEmpty() && !firstName.isNullOrBlank()){
+            firstSymbol = firstName.substring(0,1).toUpperCase()
+        }
+        if(!lastName.isNullOrEmpty() && !lastName.isNullOrBlank()){
+            lastSymbol = lastName.substring(0,1).toUpperCase()
+        }
+        result = firstSymbol + lastSymbol
+        if(result == ""){
+            return null
+        }
+        else{
+            return result
+        }
     }
 }
