2252057c098af2b8edbb18673a54dc154eab8296
Sergey Arimov <arimov@arimsoft.ru>
2019-07-01 12:41:21 +0300

    Fixed bugs in Date extensions

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -34,52 +34,108 @@ enum class TimeUnits{
     DAY
 }
 
-fun Date.humanizeDiff(date: Date): String{
-    val diff = Date().getTime() - date.getTime()
-    when(diff){
-        in 0..1*1000 -> return("только что")
-        in 1*1000+1..45*1000 -> return("несколько секунд назад")
-        in 45*1000+1..75*1000 -> return("минуту назад")
-        in 75*1000+1..45*60*1000 -> {
-            val min: Int = (diff/1000/60).toInt()
-            var minText = "минут"
-            when(min.toString().substring(min.toString().length-1,1)){
-               "1" -> minText = "минута"
-               in (2..4).toString() -> minText = "минуты"
-            }
-            return("$min $minText назад")
-        }
-        in 45*60*1000+1..75*60*1000 -> return("час назад")
-        in 75*60*1000+1..22*60*60*1000 -> {
-            val hours: Int = (diff/1000/60/60).toInt()
-            var hourText = "часов"
-            when(hours.toString().substring(hours.toString().length-1,1)){
-                "1" -> hourText = "час"
-                in (2..4).toString() -> hourText = "часа"
-            }
-            return("$hours $hourText назад")
-        }
-        in 22*60*60*1000+1..26*60*60*1000 -> return("день назад")
-        in 26*60*60*1000+1..360*24*60*60*1000 -> {
-            val days: Int = (diff/1000/60/60/24).toInt()
-            var daysText = "дней"
-            when(days.toString().substring(days.toString().length-1,1)){
-                "1" -> daysText = "день"
-                in (2..4).toString() -> daysText = "дня"
-            }
-            return("$days $daysText назад")
-        }
-        else -> return("более года назад")
+fun Date.humanizeDiff(date: Date = Date()): String{
+    val diff: Long
+    if(date.time == Date().time){
+        //сравниваем с внутренним состоянием
+        diff = this.time - Date().time
     }
+    else{
+        //сравниваем с заданным в параметре
+        diff = date.getTime() - Date().time
+    }
+
+    if(diff < 0){
+        when(diff){
+            in -1*1000..0 -> return("только что")
+            in -45*1000..-1*1000+1 -> return("несколько секунд назад")
+            in -75*1000..-45*1000+1 -> return("минуту назад")
+            in -45*60*1000..-75*1000+1 -> {
+                val min: Int = -(diff/1000/60).toInt()
+                var minText = "минут"
+                when(min.toString().substring(min.toString().length-1).toInt()){
+                    1 -> minText = "минута"
+                    in (2..4) -> minText = "минуты"
+                }
+                if(min in (11..14)) minText = "минут"
+                return("$min $minText назад")
+            }
+            in -75*60*1000..-45*60*1000+1 -> return("час назад")
+            in -22*60*60*1000..-75*60*1000+1 -> {
+                val hours: Int = -(diff/1000/60/60).toInt()
+                var hourText = "часов"
+                when(hours.toString().substring(hours.toString().length-1).toInt()){
+                    1 -> hourText = "час"
+                    in (2..4) -> hourText = "часа"
+                }
+                if(hours in (11..14)) hourText = "часов"
+                return("$hours $hourText назад")
+            }
+            in -26*60*60*1000..-22*60*60*1000+1 -> return("день назад")
+            in -360*24*60*60*1000..-26*60*60*1000+1 -> {
+                val days: Int = -(diff/1000/60/60/24).toInt()
+                var daysText = "дней"
+                when(days.toString().substring(days.toString().length-1,1).toInt()){
+                    1 -> daysText = "день"
+                    in (2..4) -> daysText = "дня"
+                }
+                if(days in (11..14)) daysText = "дней"
+                return("$days $daysText назад")
+            }
+            else -> return("более года назад")
+        }
+    }
+    else{
+        when(diff){
+            in 0..1*1000 -> return("только что")
+            in 1*1000+1..45*1000 -> return("через несколько секунд")
+            in 45*1000+1..75*1000 -> return("через минуту")
+            in 75*1000+1..45*60*1000 -> {
+                val min: Int = (diff/1000/60).toInt()
+                var minText = "минут"
+                when(min.toString().substring(min.toString().length-1).toInt()){
+                    1 -> minText = "минута"
+                    in (2..4) -> minText = "минуты"
+                }
+                if(min in (11..14)) minText = "минут"
+                return("через $min $minText")
+            }
+            in 45*60*1000..75*60*1000 -> return("через час")
+            in 75*60*1000+1..22*60*60*1000 -> {
+                val hours: Int = (diff/1000/60/60).toInt()
+                var hourText = "часов"
+                when(hours.toString().substring(hours.toString().length-1).toInt()){
+                    1 -> hourText = "час"
+                    in (2..4) -> hourText = "часа"
+                }
+                if(hours in (11..14)) hourText = "часов"
+                return("через $hours $hourText")
+            }
+            in 22*60*60*1000+1..26*60*60*1000 -> return("через день")
+            in 26*60*60*1000+1..360*24*60*60*1000 -> {
+                val days: Int = (diff/1000/60/60/24).toInt()
+                var daysText = "дней"
+                when(days.toString().substring(days.toString().length-1).toInt()){
+                    1 -> daysText = "день"
+                    in (2..4) -> daysText = "дня"
+                }
+                if(days in (11..14)) daysText = "дней"
+                return("через $days $daysText")
+            }
+            else -> return("через год и более")
+        }
+    }
+
 }
 
 /*
 0с - 1с "только что"
-1с - 45с "несколько секунд назад"
-45с - 75с "минуту назад"
-75с - 45мин "N минут назад"
-45мин - 75мин "час назад"
-75мин 22ч "N часов назад"
-22ч - 26ч "день назад"
-26ч - 360д "N дней назад"
->360д "более года назад"*/
+1с - 45с "несколько секунд назад", "через несколько секунд"
+45с - 75с "минуту назад", "через минуту"
+75с - 45мин "N минут назад", "через N минут"
+45мин - 75мин "час назад", "через час"
+75мин 22ч "N часов назад", "через N часов"
+22ч - 26ч "день назад", "через день"
+26ч - 360д "N дней назад", "через N дней"
+>360д "более года назад", "через год и более"
+*/
