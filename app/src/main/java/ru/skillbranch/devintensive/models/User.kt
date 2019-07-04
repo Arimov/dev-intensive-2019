809afc5957cdb904aa3c0c506124523e029dceb7
Sergey Arimov <arimov@arimsoft.ru>
2019-07-04 10:39:05 +0300

    yet another builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -20,7 +20,7 @@ class User (
         avatar=null
     )
 
-   private constructor(builder: Builder) : this(
+/*   private constructor(builder: Builder) : this(
         builder.id,
         builder.firstName,
         builder.lastName,
@@ -29,7 +29,7 @@ class User (
         builder.respect,
         builder.lastVisit,
         builder.isOnline
-        )
+        )*/
 
     companion object Factory{
         private var lastId : Int = -1
@@ -41,8 +41,69 @@ class User (
     }
 
 
+    class Builder {
+        private var id: String = (++Factory.lastId).toString()
+        private var firstName: String? = null
+        private var lastName: String? = null
+        private var avatar: String? = null
+        private var rating: Int = 0
+        private var respect: Int = 0
+        private var lastVisit: Date? = null
+        private var isOnline: Boolean = false
 
-    class Builder{
+        fun id(id: String): Builder {
+            this.id = id
+            return this
+        }
+
+        fun firstName(firstName: String?): Builder {
+            this.firstName = firstName
+            return this
+        }
+
+        fun lastName(lastName: String?): Builder {
+            this.lastName = lastName
+            return this
+        }
+
+        fun avatar(avatar: String?): Builder {
+            this.avatar = avatar
+            return this
+        }
+
+        fun rating(rating: Int): Builder {
+            this.rating = rating
+            return this
+        }
+
+        fun respect(respect: Int): Builder {
+            this.respect = respect
+            return this
+        }
+
+        fun lastVisit(lastVisit: Date?): Builder {
+            this.lastVisit = lastVisit
+            return this
+        }
+
+        fun isOnline(isOnline: Boolean): Builder {
+            this.isOnline = isOnline
+            return this
+        }
+
+        fun build() = User(
+            id,
+            firstName,
+            lastName,
+            avatar,
+            rating,
+            respect,
+            lastVisit,
+            isOnline
+        )
+    }
+
+    /*class Builder{
         var id: String = "${lastId++}"
             private set
 
@@ -67,14 +128,14 @@ class User (
         var isOnline : Boolean = false
             private set
 
-        fun id(id: String) = apply { this.id = id; return this }
-        fun firstName(firstName: String?) = apply { this.firstName = firstName; return this }
-        fun lastName(lastName: String?) = apply { this.lastName = lastName; return this }
-        fun avatar(avatar: String?) = apply { this.avatar = avatar; return this }
-        fun rating(rating: Int) = apply { this.rating = rating; return this }
-        fun respect(respect: Int) = apply { this.respect = respect; return this }
-        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit; return this }
-        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline; return this }
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
         fun build() = User(this)
-    }
+    }*/
 }
