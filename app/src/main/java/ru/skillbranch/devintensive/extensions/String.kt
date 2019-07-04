4fffda2f3ffdcccd69abc3cb3d57ae3e7a457688
Sergey Arimov <arimov@arimsoft.ru>
2019-07-04 07:56:44 +0300

    Fix in string extension truncate (trimEnd instead of trim)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,7 +1,5 @@
 package ru.skillbranch.devintensive.extensions
 
-import java.util.*
-
 fun String.truncate(count: Int = 16): String{
-    return this.substring(0,count).trim() + "..."
+    return this.substring(0,count).trimEnd() + "..."
 }
