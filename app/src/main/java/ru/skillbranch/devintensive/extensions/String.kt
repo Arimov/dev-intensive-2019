95562e7e55746f8c0dbc181cce6ed876a7e22832
Sergey Arimov <arimov@arimsoft.ru>
2019-07-04 10:23:49 +0300

    Fix truncate for short strings

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,5 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(count: Int = 16): String{
-    return this.substring(0,count).trimEnd() + "..."
+    if(this.toString().length <= count){
+        return this.toString().trimEnd()
+    }
+    else{
+        return this.substring(0,count).trimEnd() + "..."
+    }
 }
