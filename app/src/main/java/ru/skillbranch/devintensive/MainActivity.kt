3257a35f876cfc9db6009eae770fb633dfc14aaf
Sergey Arimov <arimov@arimsoft.ru>
2019-07-10 11:22:29 +0300

    lesson3 firs commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -0,0 +1,46 @@
+package ru.skillbranch.devintensive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    /**
+     *
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("M_MainActivity","onCreate")
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        Log.d("M_MainActivity","onRestart")
+    }
+
+    override  fun onStart(){
+        super.onStart()
+        Log.d("M_MainActivity","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("M_MainActivity","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("M_MainActivity","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("M_MainActivity","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("M_MainActivity","onDestroy")
+    }
+}
