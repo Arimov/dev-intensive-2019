9d132faee7e07c93c0f52c1e9f0c0dde6802d41b
Sergey Arimov <arimov@arimsoft.ru>
2019-07-11 14:29:54 +0300

    reformat code

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,8 +1,8 @@
 package ru.skillbranch.devintensive.models
 
-class Bender(var status:Status = Status.NORMAL, var question:Question = Question.NAME) {
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
 
-    fun askQuestion(): String = when(question){
+    fun askQuestion(): String = when (question) {
         Question.NAME -> Question.NAME.question
         Question.PROFESSION -> Question.PROFESSION.question
         Question.MATERIAL -> Question.MATERIAL.question
@@ -11,48 +11,48 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
         Question.IDLE -> Question.IDLE.question
     }
 
-    fun listenAnswer(answer:String) : Pair<String, Triple<Int,Int,Int>> {
-        return if(question.answer.contains(answer)){
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        return if (question.answer.contains(answer)) {
             question = question.nextQuestion()
             "Отлично - это правильный ответ!\n${question.question}" to status.color
-        }else{
+        } else {
             status = status.nextStatus()
             "Это не правильный ответ!\n${question.question}" to status.color
         }
     }
 
-    enum class Status(val color:Triple<Int, Int, Int>) {
-        NORMAL(Triple(255,255,255)),
-        WARNING(Triple(255,120,0)),
-        DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0)); //красный
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 255, 0)); //красный
 
-        fun nextStatus():Status{
-            return if(this.ordinal < values().lastIndex){
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
                 values()[this.ordinal + 1]
-            }else{
+            } else {
                 values()[0]
             }
         }
     }
 
-    enum class Question(val question:String, val answer:List<String>) {
-        NAME("Как меня зовут?", listOf("бендер", "bender")){
+    enum class Question(val question: String, val answer: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
         },
         PROFESSION("Назови мою профессию", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
         },
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
             override fun nextQuestion(): Question = BDAY
         },
-        BDAY("Когда меня создали?", listOf("2993")){
+        BDAY("Когда меня создали?", listOf("2993")) {
             override fun nextQuestion(): Question = SERIAL
         },
-        SERIAL("Мой серийный номер?", listOf("2716057")){
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
         },
-        IDLE("На этом все. Вопросов больше нет.", listOf()){
+        IDLE("На этом все. Вопросов больше нет.", listOf()) {
             override fun nextQuestion(): Question = IDLE
         };
 
